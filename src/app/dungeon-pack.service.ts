9fbc556035a2076df4994244a788f8faa05b65c6
Lapanxd <morgan.dumas@outlook.fr>
2023-03-22 22:59:24 +0100

    % progress bar fonctionnelle

--- src/app/dungeon-pack.service.ts
+++ src/app/dungeon-pack.service.ts
@@ -12,14 +12,22 @@ export class DungeonPackService {
       id: 0,
       packs: [1, 2, 3],
       percentage: 0
+
     },
   ];
 
-  public dungeonPacks$: BehaviorSubject<IDungeonPack[]>
+  public dungeonPacks$: BehaviorSubject<IDungeonPack[]>;
+  public dungeonPercentage$: BehaviorSubject<number>;
 
   constructor() { 
     this.dungeonPacks$ = new BehaviorSubject<IDungeonPack[]>(this.dungeonPacks);
+    this.dungeonPercentage$ = new BehaviorSubject<number>(0);
+
+    this.dungeonPacks.forEach((pack) => {
+      this.dungeonPercentage$.next(this.dungeonPercentage$.value + pack.percentage)
+    })
   }
+
 
   addNewPack(): void {
     let lastId = this.dungeonPacks.map(pack => pack.id).reduce((prev, curr) => curr > prev ? curr : prev);
@@ -29,6 +37,4 @@ export class DungeonPackService {
       percentage: 0
     });
   }
-
-  
 }
--- src/app/dungeon-settings/dungeon-settings.component.ts
+++ src/app/dungeon-settings/dungeon-settings.component.ts
@@ -11,13 +11,19 @@ import { IDungeonPack } from '../models/dungeon-pack';
 export class DungeonSettingsComponent implements OnInit {
 
   dungeonPacks!: IDungeonPack[];
+  dungeonPercentages!: number;
   dungeonPackSubscribtion!: Subscription;
+  dungeonPercentagesSubscribtion!: Subscription;
 
   constructor(private dungeonPackService: DungeonPackService) {}
 
   ngOnInit(): void {
     this.dungeonPackSubscribtion = this.dungeonPackService.dungeonPacks$.subscribe((dungeonPacks: IDungeonPack[]) => {
       this.dungeonPacks = dungeonPacks;
+    });
+
+    this.dungeonPercentagesSubscribtion = this.dungeonPackService.dungeonPercentage$.subscribe((dungeonPercentages: number) => {
+      this.dungeonPercentages = dungeonPercentages;
     });
   }
 
